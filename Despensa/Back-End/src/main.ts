014be80534a241d46b1a674738f4df678336deb0
LucasFrey-tech <lucasfrey19@gmail.com>
2025-05-10 19:35:08 -0300

    lf

--- Despensa/Back-End/src/main.ts
+++ Despensa/Back-End/src/main.ts
@@ -1,6 +1,6 @@
 import app from './modules/Productos/app';
 
-const PORT = process.env.PORT || 3000;  // Usa el mismo puerto que en app.ts
+const PORT = process.env.PORT || 3000; // Usa el mismo puerto que en app.ts
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
--- Despensa/Back-End/src/modules/Productos/producto.routes.ts
+++ Despensa/Back-End/src/modules/Productos/producto.routes.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';  // Importa Router directamente
+import { Router } from 'express'; // Importa Router directamente
 import * as productosController from './producto.controller';
 
-const router = Router();  // Usa Router sin necesidad de express.Router()
+const router = Router(); // Usa Router sin necesidad de express.Router()
 
 router.get('/', productosController.getAllProductos);
 
