aa1251e56913bcb25a7cd45067a339b3d528d51a
Nicolas <nicooperezz005@gmail.com>
2025-05-12 19:13:26 -0300

    pagina rara

--- Despensa/Front-End/src/app/page.tsx
+++ Despensa/Front-End/src/app/page.tsx
@@ -33,13 +33,17 @@ export default function TablaPersonas() {
       <table className={styles.main}>
         {productos.map(p => (
            <tr key={p.id}>
-          <td className={styles.cell}>{p.nombre}</td>
+          <td className={`${styles.cell} ${styles.flexRow}`}>
+            {p.nombre}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+            </svg>
+
+          </td>
+          
            </tr>
-
         ) )
-
         }
-        
       </table>
     </div>
   );
