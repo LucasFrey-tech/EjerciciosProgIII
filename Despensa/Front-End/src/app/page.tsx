ffbffbe09db68c5d11ef8905ec7add48a1128866
LucasFrey-tech <lucasfrey19@gmail.com>
2025-05-12 21:12:07 -0300

    No hacia falta un div, aguante el span

--- Despensa/Front-End/src/app/page.tsx
+++ Despensa/Front-End/src/app/page.tsx
@@ -33,13 +33,11 @@ export default function TablaPersonas() {
       <table className={styles.main}>
         {productos.map(p => (
            <tr key={p.id}>
-          <td className={`${styles.cell} ${styles.flexRow}`}>
-            <div className={styles.nombreProducto}>
-              {p.nombre}
-            </div>
+          <td className={styles.cell}>
+            <span className={styles.nombreProducto}>{p.nombre}</span>
             <button>
             <svg className = {styles.flechita} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-            <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+            <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
             </svg>
             </button>          
 
