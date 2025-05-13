6e71c9267e356d7dee8f2bc582ad21f643e4de39
LucasFrey-tech <lucasfrey19@gmail.com>
2025-05-12 21:18:44 -0300

    .

--- Despensa/Front-End/src/app/page.tsx
+++ Despensa/Front-End/src/app/page.tsx
@@ -31,22 +31,20 @@ export default function TablaPersonas() {
       <h1 className={styles.logo}>Despensa</h1>
       </header>
       <table className={styles.main}>
-        {productos.map(p => (
-           <tr key={p.id}>
-          <td className={styles.cell}>
-            <span className={styles.nombreProducto}>{p.nombre}</span>
-            <button>
-            <svg className = {styles.flechita} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-            <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
-            </svg>
-            </button>          
-
-        
-          </td>
-          
-           </tr>
-        ) )
-        }
+        <thead>
+          {productos.map(p => (
+            <tr key={p.id}>
+                <td className={styles.cell}>
+                  <span className={styles.nombreProducto}>{p.nombre}</span>
+                  <button>
+                    <svg className = {styles.flechita} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                    <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+                    </svg>
+                  </button>          
+              </td>
+            </tr>
+          ))}
+        </thead>
       </table>
     </div>
   );
