0388ea993f33bae609939c9aa169a20e56950e0c
Gaston <gastonpaez954@gmail.com>
2025-05-12 21:30:01 -0300

    commit page

--- Despensa/Front-End/src/app/inicio/page.tsx
+++ Despensa/Front-End/src/app/inicio/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import styles from '../Home.module.css';
 import { useEffect, useState } from 'react';
+import React from 'react';
 
 interface Producto {
   id: number;
@@ -22,7 +23,7 @@ export default function TablaPersonas() {
       .then(res => res.json())
       .then(data => {
         console.log("Datos recibido:", data);
-        setProductos(data.data);
+        setProductos(data.data); 
       })
       .catch(err => console.error('Error al hacer fetch:', err));
   }, []);
@@ -36,54 +37,69 @@ export default function TablaPersonas() {
 
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        {/*LOGO*/}
+      <header className={styles.header}>   
+        {/*LOGO*/ }  
         <h1 className={styles.logo}>Despensa</h1>
       </header>
       <table className={styles.main}>
-        <thead>
+        <tbody>
           {productos.map(p => (
-            <tr key={p.id}>
-              <td className={`${styles.cell} ${styles.flexRow}`}>
-                <div className={styles.nombreProducto}>
-                  {p.nombre}
-                </div>
-                <button onClick={() => toggleDetails(p.id)}>
-                  <svg className={styles.flechita} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                    <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
-                  </svg>
-                </button>
-              </td>
-            </tr>
+            <React.Fragment key={p.id}>
+              <tr>
+                <td className={`${styles.cell} ${styles.flexRow}`}>
+                  <div className={styles.nombreProducto}>
+                    {p.nombre}
+                  </div>
+                  <button onClick={() => toggleDetails(p.id)}>
+                    <svg 
+                      className={`${styles.flechita} ${visibleDetails[p.id] ? styles.rotated : ''}`} 
+                      xmlns="http://www.w3.org/2000/svg" 
+                      viewBox="0 0 24 24" 
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd" 
+                        d="M12 2.25a.75.75 0 0 1 .75.75v16.19l6.22-6.22a.75.75 0 1 1 1.06 1.06l-7.5 7.5a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 1 1 1.06-1.06l6.22 6.22V3a.75.75 0 0 1 .75-.75Z" 
+                        clipRule="evenodd" 
+                      />
+                    </svg>
+                  </button>          
+                </td>
+              </tr>
+              {visibleDetails[p.id] && (
+                <tr className={styles.detailRow}>
+                  <td colSpan={1}>
+                    <div className={styles.detailTableWrapper}>
+                      <table className={styles.main}>
+                        <thead className={styles.grid}>
+                          <tr>
+                            <th className={`${styles.cell} ${styles.headerCell}`}>ID</th>
+                            <th className={`${styles.cell} ${styles.headerCell}`}>Nombre</th>
+                            <th className={`${styles.cell} ${styles.headerCell}`}>Cantidad Almacenada</th>
+                            <th className={`${styles.cell} ${styles.headerCell}`}>Fecha Compra</th>
+                            <th className={`${styles.cell} ${styles.headerCell}`}>Fecha Vencimiento</th>
+                            <th className={`${styles.cell} ${styles.headerCell}`}>Categoria</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td className={styles.cell}>{p.id}</td>
+                            <td className={styles.cell}>{p.nombre}</td>
+                            <td className={styles.cell}>{p.cant_almacenada}</td>
+                            <td className={styles.cell}>{new Date(p.fecha_compra).toLocaleDateString()}</td>
+                            <td className={styles.cell}>{new Date(p.fecha_vec).toLocaleDateString()}</td>
+                            <td className={styles.cell}>{p.categoria_id}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
           ))}
-        </thead>
+        </tbody>
       </table>
-      {productos.map(p => (
-        visibleDetails[p.id] && (
-          <table key={`${p.id}-details`} className={styles.main}>
-            <thead className={styles.grid}>
-              <tr>
-                <th className={`${styles.cell} ${styles.headerCell}`}>ID</th>
-                <th className={`${styles.cell} ${styles.headerCell}`}>Nombre</th>
-                <th className={`${styles.cell} ${styles.headerCell}`}>Cantidad Almacenada</th>
-                <th className={`${styles.cell} ${styles.headerCell}`}>Fecha Compra</th>
-                <th className={`${styles.cell} ${styles.headerCell}`}>Fecha Vencimiento</th>
-                <th className={`${styles.cell} ${styles.headerCell}`}>Categoria</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className={styles.cell}>{p.id}</td>
-                <td className={styles.cell}>{p.nombre}</td>
-                <td className={styles.cell}>{p.cant_almacenada}</td>
-                <td className={styles.cell}>{new Date(p.fecha_compra).toLocaleDateString()}</td>
-                <td className={styles.cell}>{new Date(p.fecha_vec).toLocaleDateString()}</td>
-                <td className={styles.cell}>{p.categoria_id}</td>
-              </tr>
-            </tbody>
-          </table>
-        )
-      ))}
     </div>
   );
 }
